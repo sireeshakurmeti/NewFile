9dfa3156398e3bd9f58343b1b97a5c3538aa7472
sireeshakurmeti <skurmeti@keypoint-tech.com>
2022-03-15 15:57:58 +0530

    created new website

---
+++ Nav.js
@@ -0,0 +1,13 @@
+function openNav() {
+  
+  document.getElementById("mySidenav").style.width = "190px";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
+
+function collapse(){
+    document.getElementById("mySidenav").style.width="0";
+}
+
